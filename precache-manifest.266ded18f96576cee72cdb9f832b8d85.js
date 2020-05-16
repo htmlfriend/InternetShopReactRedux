36d6f9422b19bf80c0fabc1e2ad337316041f36d
yurii <yuriy.tim@yahoo.com>
2020-05-16 16:20:39 -0400

    Updates

---
+++ precache-manifest.266ded18f96576cee72cdb9f832b8d85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0c8bd4ba1e488a7f7d304a2dd6b3f6",
+    "url": "/InternetShopReactRedux/index.html"
+  },
+  {
+    "revision": "8706b542b79cc129d879",
+    "url": "/InternetShopReactRedux/static/css/main.c0e375ba.chunk.css"
+  },
+  {
+    "revision": "007c9331a412b8e06550",
+    "url": "/InternetShopReactRedux/static/js/2.b2e7513c.chunk.js"
+  },
+  {
+    "revision": "8706b542b79cc129d879",
+    "url": "/InternetShopReactRedux/static/js/main.fbabfcd4.chunk.js"
+  },
+  {
+    "revision": "dd0124874c2dd31b1440",
+    "url": "/InternetShopReactRedux/static/js/runtime~main.237abbfd.js"
+  }
+]);
